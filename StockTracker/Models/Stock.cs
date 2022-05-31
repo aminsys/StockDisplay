5d20b4f85cfba80323604c212b194fad001ac1f6
Amin <amin.yassin@live.com>
2022-05-31 22:55:48 +0200

    New branch

---
+++ StockTracker/Models/Stock.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockTracker.Models
+{
+    public class Stock
+    {
+        [Display(Name = "ID")]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Company name")]
+        public string Symbol { get; set; }
+
+        [Display(Name = "Close date")]
+        public DateTime CloseDate { get; set; }
+
+        [Display(Name = "Price at close")]
+        public decimal Close { get; set; }
+
+        [Display(Name = "Price at close - adjusted")]
+        public decimal AdjustedClose { get; set; }
+
+        [Display(Name = "Volume of traded stocks")]
+        public long Volume { get; set; }
+
+        public Stock()
+        {
+            // Placeholder
+        }
+    }
+}
